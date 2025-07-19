e036454fb4e2b572b7464797d8b73e1fbb81c0b4
GabrielFrahm <gabriel_frahm@hotmail.com>
2025-07-19 10:06:14 -0300

    fix:remove nlb

--- terraform/nlb.tf
+++ terraform/nlb.tf
@@ -1,43 +1,43 @@
-resource "aws_lb" "nlb" {
-  name               = "${var.prefix}-nlb"
-  internal           = true
-  load_balancer_type = "network"
-  subnets            = aws_subnet.private_subnets[*].id
+# resource "aws_lb" "nlb" {
+#   name               = "${var.prefix}-nlb"
+#   internal           = true
+#   load_balancer_type = "network"
+#   subnets            = aws_subnet.private_subnets[*].id
 
-  tags = {
-    Name = "${var.prefix}-nlb"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-nlb"
+#   }
+# }
 
-resource "aws_lb_target_group" "lambda_tg" {
-  name        = "${var.prefix}-lambda-tg"
-  target_type = "lambda"
-  vpc_id      = aws_vpc.new-vpc.id
+# resource "aws_lb_target_group" "lambda_tg" {
+#   name        = "${var.prefix}-lambda-tg"
+#   target_type = "lambda"
+#   vpc_id      = aws_vpc.new-vpc.id
 
-  health_check {
-    enabled             = true
-    interval            = 30
-    timeout             = 5
-    healthy_threshold   = 2
-    unhealthy_threshold = 2
-  }
+#   health_check {
+#     enabled             = true
+#     interval            = 30
+#     timeout             = 5
+#     healthy_threshold   = 2
+#     unhealthy_threshold = 2
+#   }
 
-  tags = {
-    Name = "${var.prefix}-lambda-tg"
-  }
-}
+#   tags = {
+#     Name = "${var.prefix}-lambda-tg"
+#   }
+# }
 
-resource "aws_lb_target_group_attachment" "lambda_attachment" {
-  target_group_arn = aws_lb_target_group.lambda_tg.arn
-  target_id        = aws_lambda_function.transaction.function_name
-}
+# resource "aws_lb_target_group_attachment" "lambda_attachment" {
+#   target_group_arn = aws_lb_target_group.lambda_tg.arn
+#   target_id        = aws_lambda_function.transaction.function_name
+# }
 
-resource "aws_lb_listener" "nlb_listener" {
-  load_balancer_arn = aws_lb.nlb.arn
-  port              = 80
-  protocol          = "TCP"
-  default_action {
-    type             = "forward"
-    target_group_arn = aws_lb_target_group.lambda_tg.arn
-  }
-}
+# resource "aws_lb_listener" "nlb_listener" {
+#   load_balancer_arn = aws_lb.nlb.arn
+#   port              = 80
+#   protocol          = "TCP"
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.lambda_tg.arn
+#   }
+# }
