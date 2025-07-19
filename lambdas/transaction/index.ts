6031183b9e77ec5d53cc5bc3ca66b0e15eec7900
GabrielFrahm <gabriel_frahm@hotmail.com>
2025-07-19 10:13:17 -0300

    feat: try lambda

--- lambdas/transaction/index.ts
+++ lambdas/transaction/index.ts
@@ -3,27 +3,15 @@ import { APIGatewayProxyEventV2, APIGatewayProxyResult } from 'aws-lambda';
 export const handler = async (
   event: APIGatewayProxyEventV2
 ): Promise<APIGatewayProxyResult> => {
-  try {
-    const data = event.body ? JSON.parse(event.body) : {};
-
-    console.log("Headers:", event.headers);
-    console.log("Method:", event.requestContext.http.method);
-    console.log("Payload:", data);
-
-    return {
-      statusCode: 200,
-      body: JSON.stringify({
-        success: true,
-        dataReceived: data,
-      }),
-    };
-  } catch (error) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({
-        success: false,
-        message: JSON.stringify(error),
-      }),
-    };
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: 'Transaction Lambda is working!',
+      input: event,
+    }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', 
+    },
   }
 };
