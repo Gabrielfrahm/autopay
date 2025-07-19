a6d75d8a0d3f1256bae33e7685928711339e39dc
GabrielFrahm <gabriel_frahm@hotmail.com>
2025-07-19 09:34:32 -0300

    fix: api gateway

--- terraform/api-gateway.tf
+++ terraform/api-gateway.tf
@@ -1,15 +1,14 @@
-resource "aws_apigatewayv2_vpc_link" "vpc_link" {
-  name = "${var.prefix}-vpc-link"
-  subnet_ids = aws_subnet.private_subnets[*].id
-#   security_group_ids = [aws_security_group.lambda_sg.id]
+resource "aws_apigateway_vpc_link" "vpc_link" {
+  name        = "${var.prefix}-vpc-link"
+  target_arns = [aws_lb.nlb.arn]
 }
 
 resource "aws_apigatewayv2_integration" "nlb_integration" {
-  api_id             = aws_apigatewayv2_api.http_api.id
-  integration_type   = "HTTP_PROXY"
-  integration_uri    = aws_lb_listener.nlb_listener.arn
-  integration_method = "ANY"
-  connection_type    = "VPC_LINK"
-  connection_id      = aws_apigatewayv2_vpc_link.vpc_link.id
+  api_id                 = aws_apigatewayv2_api.http_api.id
+  integration_type       = "HTTP_PROXY"
+  integration_uri        = aws_lb_listener.nlb_listener.arn
+  integration_method     = "ANY"
+  connection_type        = "VPC_LINK"
+  connection_id          = aws_apigateway_vpc_link.vpc_link.id
   payload_format_version = "1.0"
 }
